28141ac2b417a2eb49ed4e04467d54edf59904eb
silvio-galli <silvio.galli@gmail.com>
2016-02-08 19:27:04 +0100

    Modified seeds.rb so that it adds a unique post and a unique comment to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,31 @@ end
 puts "Seed Finished."
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."
+
+# Create unique Post
+unique_post_title = RandomData.random_sentence
+unique_post_body = RandomData.random_paragraph
+=begin deprecated version
+Post.find_or_create_by!(
+	title: RandomData.random_sentence,
+	body: RandomData.random_paragraph
+)
+=end
+Post.where(title: unique_post_title, body: unique_post_body).first_or_create
+
+# Create unique comment
+posts = Post.all
+unique_comment_body = RandomData.random_paragraph
+=begin deprecated version
+Comment.find_or_create_by!(
+	post: posts.sample,
+	body: unique_comment_body
+)
+=end
+Comment.where(post: posts.sample, body: unique_comment_body).first_or_create
+
+last_post = Post.last
+last_comment = Comment.last
+puts "After adding unique post we have #{Post.count} posts. The title of last unique post is: #{last_post.title}"
+puts "After adding unique comment we have #{Comment.count} comments."
+puts "Last comment is associated to post number #{last_comment.post.id}."
