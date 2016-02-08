f62e8b15b10381c329a80b5ed5c91b3c219d519f
silvio-galli <silvio.galli@gmail.com>
2016-02-08 17:05:13 +0100

    Added module RandmData and seeds.rb to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create posts
+50.times do
+	Post.create!(
+		title: RandomData.random_sentence,
+		body: RandomData.random_paragraph
+	)
+end
+posts = Post.all
+
+# Create comments
+100.times do
+	Comment.create!(
+		post: posts.sample,
+		body: RandomData.random_paragraph
+	)
+end
+
+puts "Seed Finished."
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
