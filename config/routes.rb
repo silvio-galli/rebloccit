1c135c5f3d74c80dc188940b140833b722ddc2df
silvio-galli <silvio.galli@gmail.com>
2016-02-16 11:26:46 +0100

    Added User controller new create functions + views + restyled homepage adding Sign Up link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :user, only: [:new, :create]
+  resources :users, only: [:new, :create]
 
   get 'welcome/index'
 
