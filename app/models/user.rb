9a1503a4569b2b2baa947b3af7d6b335cb692d99
silvio-galli <silvio.galli@gmail.com>
2016-02-21 08:34:04 +0100

    Added before_save callback to capitalize user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+	before_save { self.name = name.split(" ").map { |x| x.capitalize }.join(" ") }
+
 	before_save { self.email = email.downcase }
 	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, length: { minimum: 1 }, presence: true
